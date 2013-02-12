ea95756f05948fe3427a91f254cd931ecdc1e2cd
Balaji D Loganathan <info@spritle.com>
2013-02-12 23:44:16 +0530

    intial commit with Masonry

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feb23::Application.config.secret_token = '77b1a2fa5220cddd2998a412ca03ee1b5dc236316f2c59aff5c32eb29ffb00a5ff5180afcbcd68d05ec2012aa1715e4ee36e3a57a880adbbe7cfc85cc04484e2'
